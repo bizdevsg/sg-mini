2f1a5d886bfec5d4a5067b300284431c1d63e246
fptra28 <faturrahman86.fr@gmail.com>
2026-09-08 11:49:50 +0700

    update:live-chat from env

--- src/lib/tawk.ts
+++ src/lib/tawk.ts
@@ -1,6 +1,6 @@
 export const TAWK_CHAT_ENABLE_EVENT = "sgb:tawk-enable";
 
-export const TAWK_CHAT_WIDGET_URL = "https://chat.sg-berjangka.com/widget.js";
+export const TAWK_CHAT_WIDGET_URL = process.env.NEXT_WIDGET_EMBED_URL ?? "";
 
 export const TAWK_CHAT_WIDGET_ATTRIBUTES = {
   "data-site-id": "solid-gold-main",
